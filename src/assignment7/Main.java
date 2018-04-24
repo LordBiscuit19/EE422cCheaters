853752b530a4b816dce720ae6691856bdb4593ac
jenminsin <jenminsin@utexas.edu>
2018-04-24 15:14:45 -0500

    getting started
    procesing the file

--- src/assignment7/Main.java
+++ src/assignment7/Main.java
@@ -1,19 +1,32 @@
 package assignment7;
 
+import java.io.BufferedReader;
 import java.io.File;
 import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.util.HashMap;
+import java.util.Map;
 import java.util.Scanner;
+import java.util.Set;
 
 public class Main {
+	
 
-	public static void main(String[] args) {
+	public static void main(String[] args) throws Exception {
 		// TODO Auto-generated method stub
+
+		int wordPairs =  Integer.parseInt(args[1]);
 		
-		for (int i = 0; i < args.length; i++) {
-			System.out.println(args[i]);
+		Map<String , Set<File> > mapOfFiles = new HashMap<String , Set<File>>();
+		
+		File dir =new File("C:\\Users\\ronbe\\Downloads\\sm_doc_set.zip\\sm_doc_set");
+		
+		for (File file : dir.listFiles()) {
+			
+			Scanner S = new Scanner (new BufferedReader (new FileReader(dir)));
+			
+			
 		}
-		
-		
 		/*
 		File dir = new File(args[1]);
 		  File[] directoryListing = dir.listFiles();
@@ -39,5 +52,16 @@ public class Main {
 		
 		*/
 	}
-
+	
+	public Map<String , Set<File> > docMap(File original, int n){
+		Map<String, Set<File> > outputMap = null;
+		
+		
+		
+		
+		return outputMap;
+	}
+	
+	
+	
 }
