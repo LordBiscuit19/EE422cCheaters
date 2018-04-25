d3952c136b4bd7af1f5434b0209469fca10bdd93
jenminsin <jenminsin@utexas.edu>
2018-04-25 18:28:43 -0500

    team work
    team work makes the dream work

--- src/assignment7/Main.java
+++ src/assignment7/Main.java
@@ -5,7 +5,10 @@ import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.util.HashMap;
+import java.util.HashSet;
 import java.util.Map;
+import java.util.PriorityQueue;
+import java.util.Queue;
 import java.util.Scanner;
 import java.util.Set;
 
@@ -17,51 +20,90 @@ public class Main {
 
 		int wordPairs =  Integer.parseInt(args[1]);
 		
-		Map<String , Set<File> > mapOfFiles = new HashMap<String , Set<File>>();
+		HashMap<String , Set<File> > mapOfFiles = new HashMap<String , Set<File>>();
 		
-		File dir =new File("C:\\Users\\ronbe\\Downloads\\sm_doc_set.zip\\sm_doc_set");
+		File dir =new File(args[0]);
 		
 		for (File file : dir.listFiles()) {
 			
-			Scanner S = new Scanner (new BufferedReader (new FileReader(dir)));
+			Scanner scan = new Scanner (new BufferedReader (new FileReader(file)));
+			PriorityQueue<String> queue = new PriorityQueue<String>();
+			for(int i = 0; i < wordPairs; i++) {
+				if(scan.hasNext()) {
+					queue.add(removePunc(scan.next()));
+				}
+			}
 			
+			if (mapOfFiles.containsKey(queToString(queue))) {
+				mapOfFiles.get(queToString(queue)).add(file);
+			}
+			else{
+				HashSet<File> tempSet = new HashSet <File>();
+				tempSet.add(file);
+				mapOfFiles.put(queToString(queue), tempSet);
+			}
 			
+			while(scan.hasNext()) {
+				queue.remove();
+				queue.add(removePunc(scan.next()));
+				if (mapOfFiles.containsKey(queToString(queue))) {
+					mapOfFiles.get(queToString(queue)).add(file);
+				}
+				else{
+					HashSet<File> tempSet = new HashSet <File>();
+					tempSet.add(file);
+					mapOfFiles.put(queToString(queue), tempSet);
+				}
+				
+			}
+			
+			scan.close();
 		}
-		/*
-		File dir = new File(args[1]);
-		  File[] directoryListing = dir.listFiles();
-		  if (directoryListing != null) {
-		    for (File child : directoryListing) {
-		  
-		    	try {
-					Scanner sc = new Scanner(child);
-				} 
-		    	catch (FileNotFoundException e) {
-					// TODO Auto-generated catch block
-					e.printStackTrace();
-				}		    }
-		  
-		  } 
-		  
-		  else {
-		    // Handle the case where dir is not really a directory.
-		    // Checking dir.isDirectory() above would not be sufficient
-		    // to avoid race conditions with another process that deletes
-		    // directories.
-		  }
-		
-		*/
+		printMap(mapOfFiles);
 	}
 	
-	public Map<String , Set<File> > docMap(File original, int n){
-		Map<String, Set<File> > outputMap = null;
+
+	/**
+	 *This is a helper function that will take the queue containing the words
+	 *and will convert it to a string
+	 * @param wordSet is a queue of words from the document to process into a string
+	 * @return String of the queue of words 
+	 */
+	
+	public static String queToString(Queue<String> wordSet) {
+		String nWordString = "";
 		
+		for (String str : wordSet) {
+			nWordString += str;
+		}
 		
+		return nWordString;
 		
-		
-		return outputMap;
 	}
 	
+	public static String removePunc(String nWordString) {
+		String outputString="";
+		for(int i =0; i < nWordString.length(); i++) {
+			int asciiVal =(int)nWordString.charAt(i);
+			if(!((asciiVal >= 33 && asciiVal <= 47) || (asciiVal >=58 && asciiVal <= 64)||( asciiVal >=91 && asciiVal <=96)||(asciiVal>= 123 && asciiVal <= 127))) {
+				outputString+= nWordString.charAt(i);
+			}
+		}
+		return outputString;	
+	}
 	
+	public static void printMap(HashMap<String, Set<File>> mapper) {
+		Set<String> keys = mapper.keySet();
+		for (String key : keys) {
+			System.out.print(key + ": ");
+			printSet(mapper.get(key));
+			System.out.println();
+		}
+	}
 	
+	public static void printSet(Set<File> files) {
+		for (File file : files) {
+			System.out.print(file.getPath() + " ::: ");
+		}
+	}
 }
